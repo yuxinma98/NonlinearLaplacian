10ed1a834a4e6dcd1bd6145e8a8b992a0a85569f
Yuxin Ma <mayuxin98@gmail.com>
2025-05-05 23:36:29 -0400

    add computations for the predictions.

--- phase_transition.py
+++ phase_transition.py
@@ -2,7 +2,6 @@ import argparse
 import pickle
 import numpy as np
 import torch
-from tqdm import tqdm
 
 import theoretical_analysis.planted_submatrix_analysis as submatrix
 import theoretical_analysis.sparse_nnpca_analysis as gaussiannnpca
@@ -25,7 +24,7 @@ if __name__ == "__main__":
 
     # set up
     n = 2000  # Dimension of the matrix
-    N = 100  # Number of samples per beta
+    N = 500  # Number of samples per beta
     np.random.seed(0)
     betas = np.arange(0, 3, 0.05)  # Range of beta values
     generator = (
@@ -33,10 +32,31 @@ if __name__ == "__main__":
         if args.model == "gaussian_sparse_nnpca"
         else submatrix.generate_planted_matrix
     )
-    if args.sigma == "step":
+    if args.model == "gaussian_sparse_nnpca":
+        if args.sigma == "step":
+            H, theta = gaussiannnpca.H_discrete, gaussiannnpca.theta_discrete
+        else:
+            H, theta = gaussiannnpca.H, gaussiannnpca.theta
+    elif args.model == "planted_submatrix":
+        if args.sigma == "step":
+            H, theta = submatrix.H_discrete, submatrix.theta_discrete
+        else:
+            H, theta = submatrix.H, submatrix.theta
+    if args.sigma == "tanh":
+        sigma = lambda x: res.x[0] * np.tanh(res.x[1] * x)
+        sigma_image = [-res.x[0], res.x[0]]
+    elif args.sigma == "zshape":
+        sigma = lambda x: min(res.x[1], max(0, res.x[1] / res.x[0] * (x - res.x[2])))
+        sigma_image = [0, res.x[1]]
+    elif args.sigma == "step":
         results_list = [res[str(seed)][2].fun for seed in range(10)]
         argmin_seed = np.argmin(results_list)
         res = res[str(argmin_seed)][2]
+        sigma = beta_to_sigma(res.x)
+        sigma_image = None
+    edge_prediction = H(
+        z=theta(c=res.fun, sigma=sigma, sigma_image=sigma_image, tol=2e-12), sigma=sigma
+    )
 
     # Load existing results if available
     try:
@@ -72,16 +92,22 @@ if __name__ == "__main__":
                 res.x[1] / res.x[0] * (A_p_batch.sum(dim=2) - res.x[2]), min=0, max=res.x[1]
             )
         elif args.sigma == "step":
-            sigma = beta_to_sigma(res.x)
             diag_values = step_function(sigma, A_p_batch.sum(dim=2))
         L_p_batch = A_p_batch + torch.diag_embed(diag_values)
         evals, evecs = torch.linalg.eigh(L_p_batch)
         L_p_evals = evals[:, -1].numpy()
         L_p_evecs = ((evecs[:, :, -1] * y_batch).sum(dim=1) ** 2).numpy()
 
-        results[beta] = [A_p_evals, A_p_evecs, L_p_evals, L_p_evecs]
+        # Compute the theoretical prediction
+        if beta < res.fun:
+            prediction = edge_prediction
+        else:
+            prediction = H(theta(c=beta, sigma=sigma, sigma_image=sigma_image, tol=2e-12), sigma)
+
+        # Save results
+        results[beta] = [A_p_evals, A_p_evecs, L_p_evals, L_p_evecs, prediction]
         print(
-            f"beta: {beta:.2f}, A_p_evals: {A_p_evals.mean():.4f}, L_p_evals: {L_p_evals.mean():.4f}"
+            f"beta: {beta:.2f}, A_p_evals: {A_p_evals.mean():.4f}, L_p_evals: {L_p_evals.mean():.4f}, prediction: {prediction:.4f}"
         )
         pickle.dump(
             results, open(f"logs/{args.model}/{args.sigma}_top_eigen_n={n}_N={N}.pkl", "wb")
